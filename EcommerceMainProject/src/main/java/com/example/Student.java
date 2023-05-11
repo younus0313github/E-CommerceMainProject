310e9f2d39f452e34c5c5c0f4e9433b4bcf73456
Younus <younusshaikh0313@gmail.com>
2023-05-11 15:23:45 +0530

    Ecom Project

---
+++ EcommerceMainProject/src/main/java/com/example/Student.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Student {
+	public static void main(String[] args) {
+		int a[] = { 1, 2, 3, 4, 5, 6 };
+		for (int i = 0; i < a.length; i++) {
+
+		}
+		System.out.println(a.length);
+	}
+
+}
